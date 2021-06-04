94fab95983676bcd69fa5b5b0ab10aba7617930a
Enpas <enniopas2002@libero.it>
2021-06-04 16:54:27 +0200

    snack 5

---
+++ Snack 5/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Snack 5</title>
+</head>
+<body>
+
+  <?php
+
+    $text = 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eius in id reprehenderit! Provident nemo accusamus repellat. voluptatum cumque fuga, dolore blanditiis temporibus esse possimus modi beatae exercitationem quibusdam eligendi aperiam debitis molestiae eius. Quam ea molestias cupiditate possimus. assumenda neque, earum laborum eveniet, nihil, laboriosam illo eligendi error ullam facilis quia illum voluptates. ab porro? Doloribus amet suscipit minima. odit autem temporibus deleniti tenetur optio nam ab impedit sapiente delectus ex pariatur, aliquam rerum, quisquam consequuntur modi. ducimus dolorum quod at doloremque molestiae voluptatem. Deleniti debitis itaque ipsa, atque veritatis iste libero, eos dolore. nostrum rem deserunt vel quis eum!';
+
+    $arrFrasi = explode('. ', $text);
+
+    var_dump($arrFrasi);
+
+  ?>
+
+</body>
+</html>
